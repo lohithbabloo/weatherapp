0484baa3474e17dc00fe8ea2a5ddf73249e1deae
lohithbabloo <lohithkumartangeti@gmail.com>
2023-06-23 14:19:48 +0530

    made a functional searchbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import Searchbar from "./components/searchbar";
+import Inputresults from "./components/Inputresults";
 
 function App() {
+  const handlesearchchange = (searchData) => {
+    console.log(searchData);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Searchbar onSearchChange={handlesearchchange} />
+      {/* <Inputresults results={results} /> */}
     </div>
   );
 }
